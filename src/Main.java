7c40f99566f6c705b075b88184c611ccd82aebfd
kevindenishan <kevindenishan89@gmail.com>
2024-10-10 13:27:51 +0530

    Trying to understand and revise the if , if-else , switch

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,66 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
+import java.util.Scanner; // Importing the util package to the code block
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner (System.in);
-        System.out.println("Enter your name :");
-        String name = input.next();
-        System.out.println("Hi there " + name );
-    }
+//        Scanner input = new Scanner (System.in);
+//        System.out.println("Enter your name and surname :");
+//        String name = input.next();
+//        String surname = input.next();
+//        System.out.println("Hi there " + surname + " " + name );
+
+//        String name = "johnson" ;
+//        String upper_case = name.toUpperCase();
+//        System.out.println(name);
+//
+//        Scanner input = new Scanner(System.in);
+//         switch (number) {
+////            case 1:
+////                System.out.println("Monday ");
+////                break;System.out.println("Enter a number between 0-7");
+//        int number = input.nextInt();
+//
+//
+//            case 2:
+//                System.out.println("tuesday ");
+//                break;
+//            case 3:
+//                System.out.println("wednesday ");
+//                break;
+//            case 4:
+//                System.out.println("thursday  ");
+//                break;
+//            case 5:
+//                System.out.println("Friday ");
+//                break;
+//            default:
+//                System.out.println("Weekend");
+//                break;
+//
+//        }
+        int number = 0;
+        Scanner input = new Scanner(System.in);
+         switch (number) {
+             case 1:
+                   System.out.println("Monday ");
+                   break;System.out.println("Enter a number between 0-7");
+                 int number = input.nextInt();
+
+            case 2:
+                System.out.println("tuesday ");
+                break;
+            case 3:
+                System.out.println("wednesday ");
+                break;
+            case 4:
+                System.out.println("thursday  ");
+                break;
+            case 5:
+                System.out.println("Friday ");
+                break;
+            default:
+                System.out.println("Weekend");
+                break;
+
+        }    }
 }
